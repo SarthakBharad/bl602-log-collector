91cdbeb8daad3a1c3b4094181c5478c850b153c9
SarthakBharad <sarthakbharad3105@gmail.com>
2026-02-07 17:26:59 +0100

    Enhance .gitignore to exclude .der files and clean up log-related code in multiple files for improved readability and maintainability

--- suas_app_mqtt/main.cpp
+++ suas_app_mqtt/main.cpp
@@ -15,30 +15,33 @@ extern "C" {
 // Own header
 #include "wifi.h"
 
-/* main function, execution starts here */
+// main function, execution starts here 
 extern "C" void bfl_main(void) {
-  /* Define information containers for tasks */
+  // Define information containers for tasks 
   constexpr uint16_t WIFI_STACK_SIZE = 1024;
   constinit static StackType_t wifi_stack[WIFI_STACK_SIZE]{};
   constinit static StaticTask_t wifi_task{};
 
-  /* Initialize system */
+  // Initialize system 
   vInitializeBL602();
 
   // === ADD THIS LINE HERE ===
   smart_log("Booting...\r\n");
 
-  /* Start tasks */
-  /* WiFi task */
+  // Start tasks 
+  // WiFi task 
   smart_log("[SYSTEM] Starting WiFi task\r\n");
   xTaskCreateStatic(task_wifi, etl::string_view("wifi").data(), WIFI_STACK_SIZE,
                     nullptr, 16, wifi_stack, &wifi_task);
 
-  /* Start TCP/IP stack */
+  // Start TCP/IP stack 
   smart_log("[SYSTEM] Starting TCP/IP stack\r\n");
   tcpip_init(nullptr, nullptr);
 
-  /* Start scheduler */
+  // Start scheduler 
   smart_log("[SYSTEM] Starting scheduler\r\n");
   vTaskStartScheduler();
 }
+
+
+
--- suas_app_mqtt/wifi.cpp
+++ suas_app_mqtt/wifi.cpp
@@ -23,30 +23,30 @@ extern "C" {
 #include "mqtt.h"
 #include "wifi.h"
 
-/* WiFi configuration */
+// WiFi configuration 
 constinit static wifi_conf_t conf{
     .country_code = "EU",
     .channel_nums = {},
 };
 
-/* Helper function to read device tree */
+// Helper function to read device tree 
 static int get_dts_addr(etl::string_view name, uint32_t &start, uint32_t &off) {
-  /* Check we get valid data*/
+  // Check we get valid data
   if (name.empty()) {
     return -1;
   }
 
-  /* Compute device tree data */
+  // Compute device tree data 
   auto fdt = reinterpret_cast<const void *>(hal_board_get_factory_addr());
   auto offset = fdt_subnode_offset(fdt, 0, name.data());
 
-  /* Check if offset is valid*/
+  // Check if offset is valid
   if (offset <= 0) {
     log_error("%s is invalid\r\n", name);
     return -1;
   }
 
-  /* Return final data */
+  // Return final data 
   start = reinterpret_cast<uint32_t>(fdt);
   off = offset;
   return 0;
@@ -65,7 +65,7 @@ static void _configure_wifi(void) {
   }
 }
 
-/* Connect to a WiFi access point */
+// Connect to a WiFi access point 
 static void _connect_sta_wifi() {
   // Enable station mode
   auto wifi_interface = wifi_mgmr_sta_enable();
@@ -75,11 +75,11 @@ static void _connect_sta_wifi() {
                         etl::string<32>(WIFI_PW).data(), nullptr, 0, 0, 0);
   smart_log("[WIFI] Connected to a network\r\n");
 
-  /* Enable automatic reconnect */
+  // Enable automatic reconnect 
   wifi_mgmr_sta_autoconnect_enable();
 }
 
-/* React to WiFi events */
+// React to WiFi events 
 static void event_cb_wifi_event(input_event_t *event,
                                 [[gnu::unused]] void *private_data) {
   switch (event->code) {
@@ -103,12 +103,7 @@ static void event_cb_wifi_event(input_event_t *event,
     case CODE_WIFI_ON_PRE_GOT_IP:
       smart_log("[WIFI] About to receive IP address\r\n");
       break;
-    /*case CODE_WIFI_ON_GOT_IP:
-      smart_log("[WIFI] Received an IP address\r\n");
-      // Initialize MQTT connection
-      vTaskDelay(pdMS_TO_TICKS(80));
-      my_mqtt_connect();
-      break;*/
+
     case CODE_WIFI_ON_GOT_IP: {
       uint32_t ip, gateway, mask;
       wifi_mgmr_sta_ip_get(&ip, &gateway, &mask);
@@ -160,7 +155,7 @@ static void event_cb_wifi_event(input_event_t *event,
   }
 }
 
-/* Listener for key events */
+// Listener for key events 
 void event_cb_key_event(input_event_t *event,
                         [[gnu::unused]] void *private_data) {
   if (event->code == KEY_3) {
@@ -174,7 +169,7 @@ void event_cb_key_event(input_event_t *event,
   }
 }
 
-/* WiFi task */
+// WiFi task 
 void task_wifi([[gnu::unused]] void *pvParameters) {
   // Setup looprt task
   uint32_t fdt = 0, offset = 0;  // these must be uint32_t
@@ -182,49 +177,60 @@ void task_wifi([[gnu::unused]] void *pvParameters) {
   constinit static StackType_t task_looprt_stack[LOOPRT_STACK_SIZE]{};
   constinit static StaticTask_t task_looprt_task{};
 
-  /* Init looprt */
+  // Init looprt 
   looprt_start(task_looprt_stack, LOOPRT_STACK_SIZE, &task_looprt_task);
   loopset_led_hook_on_looprt();
 
-  /* Setup virtual file system */
+  // Setup virtual file system 
   vfs_init();
   vfs_device_init();
 
-  /* Setup UART */
+  // Setup UART 
   if (get_dts_addr(etl::string_view("uart"), fdt, offset) == 0) {
     vfs_uart_init(fdt, offset);
   }
 
-  /* Setup GPIO */
+  // Setup GPIO 
   if (get_dts_addr(etl::string_view("gpio"), fdt, offset) == 0) {
     fdt_button_module_init(reinterpret_cast<const void *>(fdt),
                            static_cast<int>(offset));
   }
 
-  /* Initialize command line */
+  // Initialize command line 
 #ifdef CONF_USER_ENABLE_VFS_ROMFS
   romfs_register();
 #endif
 
-  /* Initialize aos loop */
+  // Initialize aos loop 
   aos_loop_init();
 
-  /* Register event filter for key events */
+  // Register event filter for key events 
   aos_register_event_filter(EV_KEY, event_cb_key_event, nullptr);
 
-  /* Register event filter for WiFi events*/
+  // Register event filter for WiFi events
   aos_register_event_filter(EV_WIFI, event_cb_wifi_event, nullptr);
 
-  /* Start wifi firmware */
+  // Start wifi firmware 
   smart_log("[WIFI] Starting WiFi stack\r\n");
   hal_wifi_start_firmware_task();
   aos_post_event(EV_WIFI, CODE_WIFI_ON_INIT_DONE, 0);
   smart_log("[WIFI] Firmware loaded\r\n");
 
-  /*  Start aos loop */
+  //  Start aos loop 
   aos_loop_run();
 
-  /* Will hopefully never happen */
+  // Will hopefully never happen 
   smart_log("[WIFI] Exiting WiFi task - should never happen\r\n");
   vTaskDelete(nullptr);
 }
+
+
+
+
+
+
+
+
+
+
+
